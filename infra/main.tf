8eb027ea41119b87b9400907b6f7ed15f75ae0de
Helen Haveloh <h.havelohh@gmail.com>
2026-03-11 20:29:24 +0100

    added tf state as local files and added smoke test script

--- infra/main.tf
+++ infra/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
